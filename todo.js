8b5465c4118375028f57f9d6470e75144b8c763b
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-09-07 14:22:03 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -10,7 +10,12 @@ eventListeners();
 
 function eventListeners(){
     form.addEventListener("submit",addToDo);
+    document.addEventListener("DOMContentLoaded",loadAllTodosToUI);
+    secondCardBody.addEventListener("click",deleteTodo);
+    filter.addEventListener("keyup",filterTodos);
+    clearButton.addEventListener("click",clearAllTodos);
 }
+
 function addToDo(e){
     const newTodo=toDoInput.value.trim();
 
@@ -25,6 +30,59 @@ function addToDo(e){
     
     e.preventDefault();
 }
+
+function filterTodos(e){
+    const filterValue=e.target.value.toLowerCase();
+    const listItems=document.querySelectorAll(".list-group-item");
+    listItems.forEach(function(listItem){
+        const text=listItem.textContent.toLowerCase();
+        if(text.indexOf(filterValue)===-1){
+            //Bulunamadı
+            listItem.setAttribute("style","display : none !important");
+        }
+        else{
+            listItem.setAttribute("style","display : block");
+        }
+    })
+}
+
+function clearAllTodos(e){
+    if(confirm("Tümünü silmek istediğinize emin misiniz?")) {
+        //toDoList.innerHTML="";
+        while(toDoList.firstElementChild != null){
+            toDoList.removeChild(toDoList.firstElementChild);
+        }
+        localStorage.removeItem("todos");
+    }
+}
+
+function deleteTodo(e){
+    if(e.target.className===("fa fa-remove")){
+        console.log("Silme İşlemi");
+        e.target.parentElement.parentElement.remove();
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+        showAlert("success","Todo başarıyla silindi!");
+    }
+   // console.log(e.target);
+}
+
+function deleteTodoFromStorage(deletetodo){
+    let todos=getTodosFromStorage();
+    todos.forEach(function(todo,index){
+        if(todo===deletetodo){
+            todos.splice(index,1);
+        }
+    })
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
+
+function loadAllTodosToUI(){
+    let todos= getTodosFromStorage();
+    todos.forEach(function(todo){
+        addTodoToUI(todo);
+    });
+}
+
 
 function showAlert(type,message){
     const alert=document.createElement("div");
